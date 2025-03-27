79f763bb81531375cd0224ebd841666f2f5840df
Kaifas Kain <alex.malukin@mail.ru>
2025-03-27 17:39:28 +0300

    переделал метод обработки массивов с бэка

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,9 @@ export default function App() {
           <h3>Наш подход к обучению</h3>
 
           <ul>
-            <WayToTeach title={ways[0].title} description={ways[0].description} />
-            <WayToTeach {...ways[1]} />
-            <WayToTeach {...ways[2]} />
-            <WayToTeach {...ways[3]} />
+            {ways.map((way) => (
+              <WayToTeach {...way} />
+            ))}
           </ul>
         </section>
         <section>
